97bd7d94f1824ca99a29a77b9d5b7ebed3b27075
Pavan Kataria <kataria.pavan@gmail.com>
2016-08-04 18:31:44 +0100

    Search Results View MOdel and VC created

---
+++ SearchController/ViewControllers/SearchResultsView/SearchResultsViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  SearchResultsViewModel.swift
+//  SearchController
+//
+//  Created by Pavan Kataria on 04/08/2016.
+//  Copyright © 2016 Pavan Kataria. All rights reserved.
+//
+
+import Foundation
+
+class SearchResultsViewModel {
+    //Google Text Search Api
+    //Grab coordinate of the user's selected item
+    //and have that coordinate be the basis for the Feed Search
+    init(){
+        
+    }
+}
